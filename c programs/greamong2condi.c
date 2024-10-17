e4c1e9e7f81f98f08b2094cb164cc009a86f174d
Isagi7439 <shreyasghosh515@gmail.com>
2024-10-17 16:16:54 +0530

    Create greamong2condi.c

---
+++ c programs/greamong2condi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int large,a,b;
+printf("\nenter the first number");
+scanf("%d",&a);
+printf("\nenter the 2nd number");
+scanf("%d",&b);
+large=(a>b?a:b);
+printf("the larger number is %d");
+getch();
+}
